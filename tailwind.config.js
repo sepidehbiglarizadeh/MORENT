056024fd1325c447019f04c8c24640a3c7925873
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-03-04 18:34:30 +0330

    Install next and tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#F6F7F9",
+      },
+    },
+  },
+  plugins: [],
+};
