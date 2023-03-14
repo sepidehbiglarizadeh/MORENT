173ee47f64bfd0c817cc37fd9c3975c85cbc8fb7
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-03-14 18:58:41 +0330

    add ImageGallery component

--- src/pages/cars/[hashId]/[carSlug].js
+++ src/pages/cars/[hashId]/[carSlug].js
@@ -1,19 +1,33 @@
+import DesktopCategory from "@/components/Category/DesktopCategory";
+import ImageGallery from "@/components/ImageGallery/ImageGallery";
+import getAllTypesService from "@/services/getAllTypesService";
 import getOneCarService from "@/services/getOneCarService";
+import Image from "next/image";
 
-const CarDetailPage = ({car}) => {
-  console.log(car);
-  return ( 
-    <div>this is car detail page</div>
-   );
-}
- 
+const CarDetailPage = ({ car, types }) => {
+  return (
+    <section className="container mx-auto max-w-[1440px] flex">
+      <div>
+        <DesktopCategory types={types} />
+      </div>
+      <section className="px-6 mt-8 w-full">
+        <ImageGallery car={car}/>
+
+      </section>
+    </section>
+  );
+};
+
 export default CarDetailPage;
 
 export async function getServerSideProps({ req, query }) {
-  const {data}= await getOneCarService(req,query);
+  const { data } = await getOneCarService(req, query);
+  const { data: typesResult } = await getAllTypesService();
+
   return {
     props: {
-      car:data.data
+      car: data.data,
+      types: typesResult.data,
     },
   };
 }
